f0d17df7aec168ff6f5bbf8d2b1d7fec3f5c5ceb
dltn00 <130423214+dltn00@users.noreply.github.com>
2023-07-19 15:04:28 +0900

    07-19

---
+++ socar/socar.js
@@ -0,0 +1,20 @@
+const header = document.querySelector('header');
+let 이전좌표 = 0;
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 100) {
+        header.classList.add('scrolled')
+    } else {
+        header.classList.remove('scrolled');
+    }
+
+    //scroll 이벤트리스너일때
+    //스크롤을 올렸는지 내렸는지 파악하려면
+    //변수를 하나 선언 해야 한다.
+    if (이전좌표 < window.scrollY) {
+        header.classList.add('hide');
+    } else {
+        header.classList.remove('hide');
+    }
+
+    이전좌표 = window.scrollY;
+}) 
