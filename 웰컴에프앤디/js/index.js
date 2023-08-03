4ddc28b162ddd71a2f1e39c6037f712b49a245e2
isu <130423214+dltn00@users.noreply.github.com>
2023-08-03 12:55:30 +0900

    a

---
+++ 웰컴에프앤디/js/index.js
@@ -0,0 +1,5 @@
+
+
+const roundAnimation = document.querySelector('round-container');
+const innnerPlus = document.querySelector('.plus-container');
+const animationText = document.querySelector('.round-text');
