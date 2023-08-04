99fdb9440fa084f9a38d1aa90b8f8c723e41bcc4
dltn00 <130423214+dltn00@users.noreply.github.com>
2023-08-04 11:21:23 +0900

    a

--- 웰컴에프앤디/js/index.js
+++ 웰컴에프앤디/js/index.js
@@ -1,5 +1,75 @@
+const animationContainer = document.querySelector('.cover-animation');
+const animationPlus = document.querySelector('.circle-inner-plus')
+const animationText = document.querySelector('.animation-text');
+
+const template = ['Finance', 'Service', 'People', 'Tech', 'Experience', 'future', '헤헤', '아무거나', '넣는다'];
+
+const viewTextTime = 2000;
+const exportCharTime = 100;
+const animationPlayTime = viewTextTime * template.length + 2000
+
+playAnimation();
+setInterval(() => {
+    playAnimation();
+}, animationPlayTime);
 
 
-const roundAnimation = document.querySelector('round-container');
-const innnerPlus = document.querySelector('.plus-container');
-const animationText = document.querySelector('.round-text');
+function playAnimation() {
+
+    reset();
+
+    setTimeout(() => {
+
+        containerGrow();
+    }, 10);
+
+    setTimeout(() => {
+        exportPlus()
+    }, 500);
+    setTimeout(() => {
+        exportText()
+
+    }, 1000);
+
+}
+
+function reset() {
+    animationPlus.style.transition = '0s';
+    animationContainer.style.transition = '0s';
+    animationPlus.style.opacity = '0';
+    animationContainer.style.width = '20px';
+    animationText.innerText = ''
+    setTimeout(() => {
+
+        animationPlus.style.transition = '0.5s';
+        animationContainer.style.transition = '0.5s';
+    }, 1);
+}
+
+function containerGrow() {
+    animationContainer.style.width = '88px'
+}
+
+
+function exportPlus() {
+    animationPlus.style.opacity = '1';
+}
+
+function exportText() {
+
+    for (let i = 0; i < template.length; i++) {
+
+        setTimeout(() => {
+
+            animationText.innerText = ''
+            for (let j = 0; j < template[i].length; j++) {
+                setTimeout(() => {
+
+                    animationText.innerText += template[i][j];
+                }, j * exportCharTime);
+            }
+        }, i * viewTextTime);
+
+
+    }
+}
